840a4c7552680d162f8fce520af10a71ac892b60
pankaj123-ep <pankaj.epcet@gmail.com>
2026-02-19 12:02:08 +0530

    datetime fix

--- vpc-subnet.py
+++ vpc-subnet.py
@@ -59,7 +59,7 @@ def create_vpc_and_subnets(event):
             "VpcCidr": vpc_cidr,
             "Region": region,
             "Subnets": subnets,
-            "CreatedAt": datetime.now(datetime.timezone.utc).isoformat()
+            "CreatedAt": datetime.timezone.utcnow().isoformat()
         }
 
         table.put_item(Item=item)
